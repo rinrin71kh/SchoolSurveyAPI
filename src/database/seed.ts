84c537280c5c78b51c274b40606eea9a22a099c3
khuoy borin satya <khuoyborin@gmail.com>
2025-08-07 00:03:02 +0700

    seeding process

--- src/database/seed.ts
+++ src/database/seed.ts
@@ -1,8 +1,10 @@
 import { sequelize } from '../config/sequelize.config';
 import { seedLocations } from './seeds/location';
+import { seedUsers } from './seeds/user.seed';
 
 (async () => {
   await sequelize.sync();
+  await seedUsers();
   await seedLocations(); // Call individual seed functions here
   console.log('🌱 Seeding complete.');
   process.exit(0);
